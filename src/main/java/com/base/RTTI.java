4a0692e0efcb51df4d458f4d2df53fa6bdf2f956
kdxf_hdhe <hdhe@iflytek.com>
2018-01-04 16:27:34 +0800

    下溯造型--出现的运行期绑定类型列外---添加一个continue

--- src/main/java/com/base/RTTI.java
+++ src/main/java/com/base/RTTI.java
@@ -5,6 +5,14 @@ package com.base;
  */
 public class RTTI {
     public static void main(String[] args) {
+        outer://continue 和continue 标识符效果是一样的
+        for (int i=0;i<10;i++){
+            if(i==5){
+                System.out.println("可以跳出了");
+                continue outer;
+            }
+            System.out.print(i+",");
+        }
         Useful[] x = {new Useful(), new MoreUseful()};
         x[0].f();
         x[1].g();
